efddbb9c78d046262a846e3d521dab9ac623599e
Florina Jemisha <jemisha.florina@gmail>
2024-04-25 11:19:34 +0200

    update image search engine-js

--- script.js
+++ script.js
@@ -9,27 +9,45 @@ let keyword = "";
 let page = 1;
 
 async function searchImages() {
-  keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
-  const response = await fetch(url);
-  const data = await response.json();
-  const results = data.results;
+  keyword = searchBox.value.trim(); // Trim whitespace from the input
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${encodeURIComponent(
+    keyword
+  )}&client_id=${accessKey}&per_page=12`;
 
-  if (page === 1) {
-    searchResult.innerHTML = "";
-    searchBox.value = "";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+
+    if (page === 1) {
+      searchResult.innerHTML = ""; // Clear search results only when page is 1
+    }
+
+    const results = data.results;
+
+    if (results && Array.isArray(results)) {
+      results.forEach((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        image.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+      });
+    } else {
+      throw new Error("Results are undefined or not an array");
+    }
+
+    showMoreBtn.style.display = "block";
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    // Handle errors
+    alert("An error occurred while fetching data. Please try again later.");
   }
-  results.map((result) => {
-    const image = document.createElement("img");
-    image.src = result.urls.small;
-    const imageLink = document.createElement("a");
-    image.href = result.links.html;
-    imageLink.target = "_blank";
-
-    imageLink.appendChild(image);
-    searchResult.appendChild(imageLink);
-  });
-  showMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
